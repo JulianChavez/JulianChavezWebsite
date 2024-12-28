d858dbed85fddf60357ed9a5f5590c17ea425151
JulianChavez <julianchavez13@gmail.com>
2024-12-28 15:10:14 -0500

    Added a type writing effect to the intro component

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+'use client';
+import React, { use, useEffect } from 'react';
+import useTypedMsg from './typingEffect';
 
-export default function Intro() {
+const hiMessage = ["Hi There", "Hello There", "Howdy There", "Speaky Hello", "Hi Engineer"]
+
+const Intro = () => {
+    //TO-DO: Fix Line in type writing effect
+    //TO-DO: Fix tailwind class error(className -> class)
+    //TO-DO: Try to remove use-client, CSR instead of SSR
+    const typedMsg = useTypedMsg(hiMessage)
+
     return (
-        <div>
-            <div className='name' style={{ textAlign: 'center' }}>
-                <h1>Hi There, I'm Julian</h1>
-            </div>
-
-        </div>
+        <h2 className='Name' style={{ textAlign: 'center', fontSize: 'xx-large' }}>
+            <span className='blinking-cursor'> {typedMsg}</span>
+            <span>, I'm Julian</span>
+        </h2>
     )
 }
+
+export default Intro
---
+++ app/components/typingEffect.tsx
@@ -0,0 +1,54 @@
+'use client'; //idk if i really need to add this or not
+import React from 'react';
+
+enum TYPE_EFFECT {
+    WRITING,
+    PAUSING,
+    DELETING
+}
+const TYPE_INTRVL = 250
+const PAUSING_INTRVL = 1000
+const DELETING_INTRVL = 100
+
+const useTypedMsg = (messageList: String[]) => {
+    const [indexList, setIndexList] = React.useState(0)
+    const [typeEffect, setTypeEffect] = React.useState(TYPE_EFFECT.WRITING)
+    const [typed, setTyped] = React.useState('')
+    React.useEffect(() => {
+        switch (typeEffect) {
+            case TYPE_EFFECT.WRITING: {
+                const nextTypedMsg = messageList[indexList].slice(0, typed.length + 1)
+
+                if (nextTypedMsg == typed) {
+                    setTypeEffect(TYPE_EFFECT.PAUSING)
+                }
+
+                const timeout = setTimeout(() => {
+                    setTyped(nextTypedMsg)
+                }, TYPE_INTRVL)
+                return () => clearTimeout(timeout)
+            }
+            case TYPE_EFFECT.DELETING: {
+                if (!typed) {
+                    const nextIndex = indexList + 1
+                    setIndexList(messageList[nextIndex] ? nextIndex : 0)
+                    setTypeEffect(TYPE_EFFECT.WRITING)
+                }
+
+                const nextTypedMsgDelete = messageList[indexList].slice(0, typed.length - 1)
+                const timeout = setTimeout(() => {
+                    setTyped(nextTypedMsgDelete)
+                }, DELETING_INTRVL)
+                return () => clearTimeout(timeout)
+            }
+            case TYPE_EFFECT.PAUSING:
+            default:
+                const timeout = setTimeout(() => {
+                    setTypeEffect(TYPE_EFFECT.DELETING)
+                }, PAUSING_INTRVL)
+                return () => clearTimeout(timeout)
+        }
+    }, [messageList, typed, typeEffect, indexList])
+    return typed
+}
+export default useTypedMsg
