d863d8c4979c8c9b8a9212bc5d01b09cc9324c74
JulianChavez <julianchavez13@gmail.com>
2024-12-28 13:08:47 -0500

    Added header component for later additions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,19 @@
+import React from 'react';
 import Intro from '@/app/components/intro';
 import About from '@/app/components/about';
-import React from 'react';
+import Header from '@/app/components/header';
 
 export default function Home() {
     return (
         <div>
-            <div>
+            <div className='Header-comp'>
+                <Header />
+            </div>
+            <div className='Intro-comp'>
                 <Intro />
             </div>
-            <About />
-            <div>
-
+            <div className='About-comp'>
+                <About />
             </div>
         </div>
 
---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    //TO-DO: Header component save for later when needed to add navi
+    return (
+        <div className='Header' style={{ width: '100%', height: '100%', backgroundColor: 'black', display: 'inline-block' }}>
+        </div>
+    )
+}
