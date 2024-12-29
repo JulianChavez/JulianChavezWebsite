958ddf41e4b6b76aeb9d26bcf670f8b459d0a340
JulianChavez <julianchavez13@gmail.com>
2024-12-29 16:09:25 -0500

    Update footer comp with links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,12 +4,12 @@ export default function Footer() {
     //Left side, pic of chachi
     //right side links to(email, linkedin)
     return (
-        <div>
+        <div className='flex w-full justify-between'>
             <div>
-                chachipic
             </div>
-            <div>
-
+            <div className='flex gap-4'>
+                <span><a href='mailto:jchavez3@umbc.edu'>Email</a></span>
+                <span><a href='https://www.linkedin.com/in/julian-chavez-b82a13254/'>LinkedIn</a></span>
             </div>
         </div>
     )
