bcd70d8c6fcb04a35d28461a63b2558120b7ae9d
JulianChavez <julianchavez13@gmail.com>
2025-03-01 09:33:18 -0500

    Added images for the planets, update test_page with cursor test

--- app/test_page_x/page.tsx
+++ app/test_page_x/page.tsx
@@ -2,6 +2,20 @@
 
 import React from 'react';
 import * as THREE from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
+import { Font, FontLoader } from 'three/examples/jsm/Addons.js';
+
+/*Import planet textures*/
+import sunTexture from '@planetTextures/sunmap.jpg';
+import mecuryTexture from '@planetTextures/mercurymap.jpg';
+import venusTexture from '@planetTextures/venusmap.jpg';
+import earthTexture from '@planetTextures/earthmap1k.jpg';
+import marsTexture from '@planetTextures/mars_1k_color.jpg';
+import jupiterTexture from '@planetTextures/jupitermap.jpg';
+import satureTexture from '@planetTextures/saturnmap.jpg';
+import uranusTexture from '@planetTextures/uranusmap.jpg';
+import neptunreTexture from '@planetTextures/neptunemap.jpg';
+import plutoTexture from '@planetTextures/plutomap1k.jpg';
 
 
 function Home() {
@@ -15,30 +29,39 @@ function Home() {
         renderer.setSize(window.innerWidth, window.innerHeight);
 
         refContainer.current && refContainer.current.appendChild(renderer.domElement);
+        const loader = new FontLoader();
+        loader.load('fonts/helvetiker_regular.typeface.json', function (font) {
+            const text_test = new TextGeometry('test', {
+                font: font,
+                size: 80,
+                depth: 5,
+            })
+        })
 
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 'white' });
-        var material_two = new THREE.MeshBasicMaterial({ color: 'red' })
-        var cube = new THREE.Mesh(geometry, material);
-        var cube_two = new THREE.Mesh(geometry, material_two);
-        cube.position.set(8, 0, 0)
-        cube_two.position.set(-8, 0, 0)
-        scene.add(cube);
-        scene.add(cube_two);
+
+        const textureLoader = new THREE.TextureLoader();
+        const sunGeo = new THREE.SphereGeometry(2, 30, 30);
+        const sunMat = new THREE.MeshBasicMaterial({
+            map: textureLoader.load(sunTexture.src)
+        });
+
+        var sun = new THREE.Mesh(sunGeo, sunMat)
+        scene.add(sun);
         //axis helper
         const axesHelper = new THREE.AxesHelper(10);
         scene.add(axesHelper)
         //Test for vercel
         camera.position.z = 10;
-        var animate = function () {
-            requestAnimationFrame(animate);
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            cube_two.rotation.x += 0.1;
-            cube_two.rotation.y += 0.1;
-            renderer.render(scene, camera);
-        };
-        animate();
+        renderer.render(scene, camera)
+        // var animate = function () {
+        //     requestAnimationFrame(animate);
+        //     cube.rotation.x += 0.01;
+        //     cube.rotation.y += 0.01;
+        //     cube_two.rotation.x += 0.1;
+        //     cube_two.rotation.y += 0.1;
+        //     renderer.render(scene, camera);
+        // };
+        // animate();
     }, []);
 
     return (
